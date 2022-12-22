93847d383b03e925688b4a16e98bcfc85578fffd
Rhythm <rhythm_2019@163.com>
2022-12-22 12:15:18 +0800

    [feat] 给商铺查询、商铺类型查询添加缓存

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,10 +1,19 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisConstants;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import sun.nio.cs.ext.SJIS;
+
+import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
@@ -17,4 +26,24 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+    @Override
+    public Result queryById(Long id) {
+        // 先查看 Redis 中是否存在缓存
+        String shopKey = RedisConstants.CACHE_SHOP_KEY + id;
+        String shopStr = stringRedisTemplate.opsForValue().get(shopKey);
+
+        if (StrUtil.isNotBlank(shopStr)) {
+            return Result.ok(JSONUtil.toBean(shopStr, Shop.class));
+        }
+
+        Shop shop = this.getById(id);
+        if (shop == null) {
+            return Result.fail("商家不存在");
+        }
+        stringRedisTemplate.opsForValue().set(shopKey, JSONUtil.toJsonStr(shop), RedisConstants.CACHE_SHOP_TTL, TimeUnit.MINUTES);
+
+        return Result.ok(shop);
+    }
 }
--- src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,10 +1,22 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisConstants;
+import org.springframework.data.redis.core.DefaultTypedTuple;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -17,4 +29,25 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+    @Override
+    public Result queryTypeList() {
+        // 读取缓存
+        Set<String> shopStrSet = stringRedisTemplate.opsForZSet().rangeByScore(RedisConstants.SHOP_TYPE_LIST_KEY, 0, -1);
+        if (shopStrSet != null && !shopStrSet.isEmpty()) {
+            return Result.ok(shopStrSet.stream()
+                    .map(shopStr -> JSONUtil.toBean(shopStr, ShopType.class))
+                    .collect(Collectors.toList()));
+        }
+
+        List<ShopType> shopTypeList = query().orderByAsc("sort").list();
+        Set<ZSetOperations.TypedTuple<String>> typedTupleSet = shopTypeList.stream()
+                .map(shopType -> new DefaultTypedTuple<>(JSONUtil.toJsonStr(shopType), Double.valueOf(shopType.getSort())))
+                .collect(Collectors.toSet());
+        stringRedisTemplate.opsForZSet().add(RedisConstants.SHOP_TYPE_LIST_KEY, typedTupleSet);
+        stringRedisTemplate.expire(RedisConstants.SHOP_TYPE_LIST_KEY, RedisConstants.SHOP_TYPE_LIST_TTL, TimeUnit.MINUTES);
+
+        return Result.ok(shopTypeList);
+    }
 }

537b8c643791805d97cae42123097fa6547156cc
Rhythm <rhythm_2019@163.com>
2022-12-28 00:52:04 +0800

    [feat] 完成点赞和点赞排行榜功能

--- src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
@@ -1,10 +1,26 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.Blog;
+import com.hmdp.entity.User;
 import com.hmdp.mapper.BlogMapper;
 import com.hmdp.service.IBlogService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.service.IUserService;
+import com.hmdp.utils.RedisConstants;
+import com.hmdp.utils.SystemConstants;
+import com.hmdp.utils.UserHolder;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -17,4 +33,86 @@ import org.springframework.stereotype.Service;
 @Service
 public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IBlogService {
 
+    @Resource
+    private IUserService userService;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result queryHotBlogs(Integer current) {
+
+        // 根据用户查询
+        Page<Blog> page = query()
+                .orderByDesc("liked")
+                .page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
+        // 获取当前页数据
+        List<Blog> records = page.getRecords();
+        // 查询用户
+        records.forEach(this::fillUserInfoToBlog);
+        return Result.ok(records);
+    }
+
+    @Override
+    public Result queryBlogById(String id) {
+        Blog blog = this.getById(id);
+        if (blog == null) {
+            return Result.fail("没有找到文章");
+        }
+        fillUserInfoToBlog(blog);
+        return Result.ok(blog);
+    }
+
+    @Override
+    public void makeLike(Long id, Long userId) {
+
+        if (hadLike(id, userId)) {
+            // 取消点赞
+            boolean isSuccess = this.update()
+                    .setSql("liked = liked - 1").eq("id", id).update();
+            if (isSuccess) {
+                stringRedisTemplate.opsForZSet().remove(RedisConstants.BLOG_LIKED_KEY + id, userId.toString());
+            }
+        } else {
+            // 点赞
+            boolean isSuccess = this.update()
+                    .setSql("liked = liked + 1").eq("id", id).update();
+            if (isSuccess) {
+                stringRedisTemplate.opsForZSet().add(RedisConstants.BLOG_LIKED_KEY + id, userId.toString(), System.currentTimeMillis());
+            }
+        }
+
+    }
+
+    @Override
+    public Result getLikeUserList(String id) {
+        Set<String> userIdSet = stringRedisTemplate.opsForZSet().range(RedisConstants.BLOG_LIKED_KEY + id, 0, 4);
+        if (userIdSet == null) {
+            return Result.ok();
+        }
+
+        List<UserDTO> userDTOList = userService.query().in("id", userIdSet)
+                .last(String.format("ORDER BY FIELD(id, %s)", StrUtil.join(",", userIdSet)))
+                .list()
+                .stream()
+                .map(user -> BeanUtil.copyProperties(user, UserDTO.class))
+                .collect(Collectors.toList());
+        return Result.ok(userDTOList);
+    }
+
+    private boolean hadLike(Long blogId, Long userId) {
+        return stringRedisTemplate.opsForZSet().score(RedisConstants.BLOG_LIKED_KEY + blogId, userId.toString()) != null;
+    }
+
+    private void fillUserInfoToBlog(Blog blog) {
+        Long userId = blog.getUserId();
+        User user = userService.getById(userId);
+        blog.setName(user.getNickName());
+        blog.setIcon(user.getIcon());
+
+        UserDTO userDTO = UserHolder.getUser();
+        if (userDTO != null) {
+            blog.setIsLike(this.hadLike(blog.getId(), userDTO.getId()));
+        }
+    }
 }
